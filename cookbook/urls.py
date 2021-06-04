1f027ee812435d8cdd617e310d181dbe97d6fa7e
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-04 17:36:12 -0400

    create views using rest_framework, and specify URL path for API

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from django.views.decorators.csrf import csrf_exempt
+from ingredients import views
 
 from graphene_django.views import GraphQLView
 
-urlpatterns = [path("admin/", admin.site.urls), path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)))]
+router = routers.DefaultRouter()
+router.register(r"categories", views.CategoryView, "category")
+router.register(r"ingredients", views.IngredientView, "ingredient")
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("api/", include(router.urls)),
+]
--- ingredients/views.py
+++ ingredients/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CategorySerializer, IngredientSerializer
+from .models import Category, Ingredient
 
 # Create your views here.
+# viewsets base class provides implementation for CRUD operations by default
+class CategoryView(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class IngredientView(viewsets.ModelViewSet):
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
+
